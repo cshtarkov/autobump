405134bc2c637707ba9923d0c0bb5e7f07fce114
Christian Shtarkov <cshtarkov@gmail.com>
2016-10-02 22:32:26 +0100

    Identify changes by path, not name

--- autobump/core.py
+++ autobump/core.py
@@ -39,13 +39,15 @@ def _report_change(change, name):
     print(name + ": " + change.value)
 
 
-def _compare_properties(a_prop, b_prop):
+def _compare_properties(a_prop, b_prop, path):
     """Compare two code properties which have the same name.
 
     Return a Bump enum based on whether
     there was a major, minor, patch or no change."""
     assert a_prop.name == b_prop.name, "Shouldn't compare properties with different names."
     assert type(a_prop) is type(b_prop), "Shouldn't compare properties of different types."
+
+    path = path + "." + a_prop.name
 
     highestBump = Bump.patch  # Biggest bump encountered so far.
 
@@ -58,10 +60,10 @@ def _compare_properties(a_prop, b_prop):
     if hasattr(a_prop, "visibility"):
         assert hasattr(b_prop, "visibility"), "Should never happen: comparing properties when one has 'visibility' and other doesn't."
         if a_prop.visibility == Visibility.public and b_prop.visibility != Visibility.public:
-            _report_change(Change.visibility_from_public_to_nonpublic, a_prop.name)
+            _report_change(Change.visibility_from_public_to_nonpublic, path)
             _report_bump(Bump.major)
         if a_prop.visibility != Visibility.public and b_prop.visibility == Visibility.public:
-            _report_change(Change.visibility_became_public, a_prop.name)
+            _report_change(Change.visibility_became_public, path)
             _report_bump(Bump.minor)
 
     # Compare default values
@@ -71,18 +73,18 @@ def _compare_properties(a_prop, b_prop):
             # If default values have changed, but the properties aren't public
             # it's definitely just a patch change.
             if a_prop.default_value != b_prop.default_value:
-                _report_change(Change.changed_default_value, a_prop.name)
+                _report_change(Change.changed_default_value, path)
                 _report_bump(Bump.patch)
         else:
             # Handle case when the current property is public.
             if a_prop.default_value is None and b_prop.default_value is not None:
-                _report_change(Change.introduced_default_value, a_prop.name)
+                _report_change(Change.introduced_default_value, path)
                 # TODO: What bump is this?
             elif a_prop.default_value is not None and b_prop.default_value is None:
-                _report_change(Change.removed_default_value, a_prop.name)
+                _report_change(Change.removed_default_value, path)
                 _report_bump(Bump.major)
             elif a_prop.default_value != b_prop.default_value:
-                _report_change(Change.changed_default_value, a_prop.name)
+                _report_change(Change.changed_default_value, path)
                 _report_bump(Bump.patch)
 
     # Compare inner properties recursively
@@ -96,15 +98,15 @@ def _compare_properties(a_prop, b_prop):
 
             if ki not in a_inner:
                 # Handle case when a property was added.
-                _report_change(Change.property_was_introduced, b_inner[ki].name)
+                _report_change(Change.property_was_introduced, path + "." + b_inner[ki].name)
                 _report_bump(Bump.patch)
                 if hasattr(b_inner[ki], "visibility") and b_inner[ki].visibility == Visibility.public:
                     # Handle case when a visible property was added.
-                    _report_change(Change.visibility_became_public, b_inner[ki].name)
+                    _report_change(Change.visibility_became_public, path + "." + b_inner[ki].name)
                     _report_bump(Bump.minor)
                 if isinstance(b_inner[ki], Parameter):
                     # Handle case when function signature has changed.
-                    _report_change(Change.parameter_added_to_signature, b_inner[ki].name)
+                    _report_change(Change.parameter_added_to_signature, path + "." + b_inner[ki].name)
                     _report_bump(Bump.major)
                 continue
 
@@ -114,16 +116,16 @@ def _compare_properties(a_prop, b_prop):
                 _report_bump(Bump.patch)
                 if hasattr(a_inner[ki], "visibility") and a_inner[ki].visibility == Visibility.public:
                     # Handle case when a visible property was removed.
-                    _report_change(Change.visibility_from_public_to_nonpublic, a_inner[ki].name)
+                    _report_change(Change.visibility_from_public_to_nonpublic, path + "." + a_inner[ki].name)
                     _report_bump(Bump.major)
                 if isinstance(a_inner[ki], Parameter):
                     # Handle case when function signature has changed.
-                    _report_change(Change.parameter_removed_from_signature, a_inner[ki].name)
+                    _report_change(Change.parameter_removed_from_signature, path + "." + a_inner[ki].name)
                     _report_bump(Bump.major)
                 continue
 
             # Handle general case when a property may have changed.
-            _report_bump(_compare_properties(a_inner[ki], b_inner[ki]))
+            _report_bump(_compare_properties(a_inner[ki], b_inner[ki], path))
 
     return highestBump
 
@@ -136,4 +138,4 @@ def compare_codebases(a_units, b_units):
     # Represent both codebases as a single unit, and compare that.
     a_unit = Unit("codebase", [], [], a_units)
     b_unit = Unit("codebase", [], [], b_units)
-    return _compare_properties(a_unit, b_unit)
+    return _compare_properties(a_unit, b_unit, "root")
