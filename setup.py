d7fb8fc50dc7955261bf226b0ed5ff50f96e7c7f
Christian Shtarkov <cshtarkov@gmail.com>
2017-01-29 15:39:02 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if sys.version_info < (3, 5):
     sys.exit("Python 3.5+ is required for Autobump.")
 
 setup(name="autobump",
-      version="0.2.0",
+      version="0.3.0",
       description="Automatic semantic versioning of projects",
       url="https://github.com/cshtarkov/autobump",
       author="Christian Shtarkov",
